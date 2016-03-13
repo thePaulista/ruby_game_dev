70660ec0dd95188c09d8e1daa8603240f743352e
Hedy <hedywoo@gmail.com>
2016-03-13 13:14:29 -0500

    correct file

--- whack_a_phony_game/whack_dephony.rb
+++ whack_a_phony_game/whack_dephony.rb
@@ -1,9 +1,19 @@
 require 'gosu'
+require_relative 'ruby.png'
 
 class WhackDePhony < Gosu::Window
   def initialize
     super(800,600)
     self.caption = "Whack de Phony!"
+    @image = Gosu::Image.new('ruby.png')
+    @x = 200
+    @y = 200
+    @width = 50
+    @height = 43
+  end
+
+  def draw()
+    @image.draw(@x - @width / 2, @y - @height / 2, 1)
   end
 end
 
