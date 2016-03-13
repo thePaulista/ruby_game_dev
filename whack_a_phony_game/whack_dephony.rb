9a393bc7a190b476f10b676832688796f71e433d
Hedy <hedywoo@gmail.com>
2016-03-13 14:16:52 -0500

    ruby moves side to side and blinks now

--- whack_a_phony_game/whack_dephony.rb
+++ whack_a_phony_game/whack_dephony.rb
@@ -9,10 +9,26 @@ class WhackDePhony < Gosu::Window
     @y = 200
     @width = 50
     @height = 43
+    @velocity_x = 5
+    @velocity_y = 5
+    @visible = 0
   end
 
   def draw()
-    @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    if @visible > 0
+      @image.draw(@x - @width / 2, @y - @height / 2, 1)
+    end
+  end
+
+  def update()
+    @x += @velocity_x
+    @y ++ @velocity_y
+
+    @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
+    @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
+
+    @visible -= 1
+    @visible = 30 if @visible < -10 && rand < 0.05
   end
 end
 
