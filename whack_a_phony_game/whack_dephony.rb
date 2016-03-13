24692100f5acacb6c4da624053d3b23e7ff77cb3
Hedy <hedywoo@gmail.com>
2016-03-13 16:18:12 -0500

    added score component

--- whack_a_phony_game/whack_dephony.rb
+++ whack_a_phony_game/whack_dephony.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require 'pry'
 
 class WhackDePhony < Gosu::Window
   def initialize
@@ -12,23 +13,48 @@ class WhackDePhony < Gosu::Window
     @velocity_x = 5
     @velocity_y = 5
     @visible = 0
+    @hammer_image = Gosu::Image.new('hammer.png')
+    @hit = 0
+    @font = Gosu::Font.new(30)
+    @score = 0
   end
 
   def draw()
     if @visible > 0
       @image.draw(@x - @width / 2, @y - @height / 2, 1)
     end
+    @hammer_image.draw(mouse_x - 40, mouse_y - 10, 1)
+    if @hit == 0
+      c = Gosu::Color::NONE
+    elsif @hit == 1
+      c = Gosu::Color::GREEN
+    elsif @hit == -1
+      c = Gosu::Color::RED
+    end
+    draw_quad(0, 0, c, 800, 0, c, 800, 600, c, 0, 600, c)
+    @hit = 0
+    @font.draw(@score.to_s, 700, 20, 2)
   end
 
   def update()
     @x += @velocity_x
     @y ++ @velocity_y
-
     @velocity_x *= -1 if @x + @width / 2 > 800 || @x - @width / 2 < 0
     @velocity_y *= -1 if @y + @height / 2 > 600 || @y - @height / 2 < 0
-
     @visible -= 1
     @visible = 30 if @visible < -10 && rand < 0.05
+  end
+
+  def button_down(id)
+    if (id == Gosu::MsLeft) #left mouse button
+      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+        @hit = 1
+        @score += 3
+      else
+        @hit = -1
+        @score -= 1
+      end
+    end
   end
 end
 
